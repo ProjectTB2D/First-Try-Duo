49ac948a339578576c05ae604be1e0ae3c8c1b1e
jordan <aupetit.jordan@gmail.com>
2012-10-03 20:10:44 +0200

    Create Project Test IA

---
+++ testIA/main.cpp
@@ -0,0 +1,23 @@
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+
+
+
+const Vector2f WINDOW_SIZE(1280,600);
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
